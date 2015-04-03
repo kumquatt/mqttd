6771831f362df04e93ba94d505dbd40c87d4545b
Joseph Yang <before30@gmail.com>
2015-04-03 23:59:16 +0900

    Baseline

---
+++ build.sbt
@@ -0,0 +1,6 @@
+name := "kumquatt"
+
+version := "1.0"
+
+scalaVersion := "2.11.6"
+    
---
+++ src/main/scala-2.11/plantae/citrus/Kumquat.scala
@@ -0,0 +1,8 @@
+package plantae.citrus
+
+object Kumquat {
+
+  def main(args: Array[String]): Unit = {
+    println("Hello world")
+  }
+}
