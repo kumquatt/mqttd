53cff235b96abb428644f802cbb70f0e9ec3bc0d
Joseph Yang <before30@gmail.com>
2015-04-06 00:34:28 +0900

    Add connect control

--- src/main/scala-2.11/plantae/citrus/mqtt/domains/Connect.scala
+++ src/main/scala-2.11/plantae/citrus/mqtt/domains/Connect.scala
@@ -1,6 +1,24 @@
 package plantae.citrus.mqtt.domains
 
-import plantae.citrus.mqtt.{ControlPacket, FixedHeader, Message}
+import akka.util.ByteString
+import plantae.citrus.mqtt._
+
+object Connect {
+
+  val MinValue = 0
+  val MaxValue = 268435455
+
+  def decode(bytes: ByteString) : CONNECT = {
+    val fixedHeader = FixedHeader(ControlPacketType.CONNECT, false, QosLevel.AtLeastOnce, false, 0)
+    val protocolName = "TET"
+    val protocolVersion : Short = 1
+    val cleanSession = false
+    val keepAliveInSecodes = 10
+    val clientId = "id"
+
+    CONNECT(fixedHeader, protocolName, protocolVersion, cleanSession, keepAliveInSecodes, clientId)
+  }
+}
 
 case class CONNECT(header: FixedHeader,
                    protocolName: String,
@@ -8,9 +26,9 @@ case class CONNECT(header: FixedHeader,
                    cleanSession: Boolean,
                    keepAliveInSeconds: Int,
                    clientId: String,
-                   username: Option[String],
-                   password: Option[String],
-                   willMessage: Option[Message]) extends ControlPacket
+                   username: Option[String] = None,
+                   password: Option[String] = None,
+                   willMessage: Option[Message] = None) extends ControlPacket
 
 // TODO: need to implement
 case object CONACK extends ControlPacket
