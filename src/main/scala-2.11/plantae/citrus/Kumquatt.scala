34ce13ab34fbc3317fdfaa26347a0c19aa1899da
wonsuk.yang <before30@gmail.com>
2015-04-21 13:36:45 +0900

    change some name of classes

---
+++ src/main/scala-2.11/plantae/citrus/Kumquatt.scala
@@ -0,0 +1,26 @@
+package plantae.citrus
+
+import akka.actor.{Props, ActorSystem}
+import com.typesafe.config.ConfigFactory
+import plantae.citrus.exercise.{DISCONNECT_MQTT, CONNECT_MQTT, PahoClient, StartUpMessage}
+import plantae.citrus.mqtt.actors.KumquattServer
+
+
+object Kumquatt {
+
+  def main(args: Array[String]): Unit = {
+
+
+    val system = ActorSystem("kumquatt", ConfigFactory.load.getConfig("mqtt-broker"))
+    val server = system.actorOf(Props[KumquattServer], name = "kumquatt")
+    server ! StartUpMessage
+
+    val paho = system.actorOf(Props[PahoClient], name = "mqttClient")
+    paho ! CONNECT_MQTT
+    Thread.sleep(10000)
+    paho ! DISCONNECT_MQTT
+
+  }
+}
+
+
