e438a720f00984faae06819f3cb27d1e9350a80c
wonsuk.yang <before30@gmail.com>
2015-05-18 13:07:02 +0900

    * delete some useless files

--- src/main/scala-2.11/plantae/citrus/plugin/Webadmin.scala
+++ src/main/scala-2.11/plantae/citrus/plugin/Webadmin.scala
@@ -1,21 +1,15 @@
 package plantae.citrus.plugin
 
-import plantae.citrus.mqtt.actors.SystemRoot
-import spray.http.MediaTypes
-
-import scala.concurrent.duration._
-
-import akka.actor.{ Actor, ActorLogging, ActorSystem, Props }
+import akka.actor.{Actor, ActorLogging, Props}
 import akka.io.IO
 import akka.pattern.ask
 import akka.util.Timeout
+import plantae.citrus.mqtt.actors.SystemRoot
 import spray.can.Http
-import spray.httpx.Json4sSupport
+import spray.http.MediaTypes._
 import spray.routing._
-import spray.can.server.Stats
-import spray.http.StatusCodes._
-import spray.httpx.marshalling.Marshaller
-import MediaTypes._
+
+import scala.concurrent.duration._
 
 
 class WebPlugin extends Actor with WebAdmin with ActorLogging {
@@ -30,12 +24,12 @@ class WebPlugin extends Actor with WebAdmin with ActorLogging {
   def receive = runRoute(myRoute)
 }
 
-case class Foo(bar: String)
-
 trait WebAdmin extends HttpService {
+
   import WorkerActor._
 
   implicit def executionContext = actorRefFactory.dispatcher
+
   implicit val timeout = Timeout(5 seconds)
 
   val worker = actorRefFactory.actorOf(Props[WorkerActor], "worker")
@@ -53,28 +47,35 @@ trait WebAdmin extends HttpService {
     }
 
   def getStatus = {
-     {(worker ? "status").mapTo[Ok].map(result => s"<html><body>sessions : ${result.sessions}</br> " +
-       s"connections : ${result.connections}</br> " +
-       s"topics : ${result.topics}</body></html>" ).recover{case _ => "error in get topics"}}
+    {
+      (worker ? "status").mapTo[Ok].map(result => s"<html><body>sessions : ${result.sessions}</br> " +
+        s"connections : ${result.connections}</br> " +
+        s"topics : ${result.topics}</body></html>").recover { case _ => "error in get topics" }
+    }
   }
 
 }
 
 object WorkerActor {
+
   trait WorkerResult
-  case class Ok(sessions: Int, connections: Int, topics: Int ) extends WorkerResult
+
+  case class Ok(sessions: Int, connections: Int, topics: Int) extends WorkerResult
+
   case class Error(reason: String) extends WorkerResult
+
 }
 
 class WorkerActor extends Actor with ActorLogging {
+
   import WorkerActor._
 
   def receive = {
     case "status" => {
       log.info("ask session!!!!")
+
       sender ! Ok(util.Random.nextInt(1000), util.Random.nextInt(1000), util.Random.nextInt(1000))
     }
     case _ => Error("no method")
   }
 }
-
